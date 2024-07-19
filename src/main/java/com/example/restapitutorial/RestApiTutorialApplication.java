83db98e1376a852feb69465915565b2744b8d834
Jan-Hendrik Ruberg <Jan-Hendrik.Ruberg@t-online.de>
2024-07-19 15:45:43 +0200

    SpringBootTest

--- src/main/java/com/example/restapitutorial/RestApiTutorialApplication.java
+++ src/main/java/com/example/restapitutorial/RestApiTutorialApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApiTutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiTutorialApplication.class, args);
-
 	}
 }
--- src/test/java/com/example/restapitutorial/RestApiTutorialApplicationTests.java
+++ src/test/java/com/example/restapitutorial/RestApiTutorialApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.restapitutorial;
 
-import org.junit.jupiter.api.Test;
+import com.example.restapitutorial.products.Product;
+import com.example.restapitutorial.products.ProductRepository;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class RestApiTutorialApplicationTests {
 
+	@Autowired
+	private ProductRepository productRepository;
+
+	private static final String productname = "Shoe";
+
+
+	@BeforeEach
+	public void addProductsToDb() {
+		Product product = new Product();
+		product.setName(productname);
+		product.setCostInEuro(23);
+
+		productRepository.save(product);
+	}
+
+	@AfterEach
+	public void clearProductDb() {
+		productRepository.deleteAll();
+	}
+
 	@Test
-	void contextLoads() {
+	void testGetRequest() {
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		ResponseEntity<Product> productEntity = restTemplate.getForEntity("http://localhost:8080/products/1", Product.class);
+		Assertions.assertEquals(productEntity.getStatusCode(), HttpStatus.OK);
+		Assertions.assertNotNull(productEntity.getBody());
+		Assertions.assertEquals(productEntity.getBody().getName(), productname);
 	}
 
 }
