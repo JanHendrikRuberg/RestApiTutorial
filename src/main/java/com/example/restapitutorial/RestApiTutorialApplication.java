2c61f8b1602dfdb16f0958bacf9ce666d518f7fd
Jan-Hendrik Ruberg <Jan-Hendrik.Ruberg@t-online.de>
2024-07-19 14:24:57 +0200

    Init

--- src/main/java/com/example/restapitutorial/RestApiTutorialApplication.java
+++ src/main/java/com/example/restapitutorial/RestApiTutorialApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiTutorialApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiTutorialApplication.class, args);
-	}
 
+	}
 }
